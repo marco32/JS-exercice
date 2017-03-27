c3d94fa8ee58ca5e982727212f5459e9aa465961
marco32 <marcofrosi32@gmail.com>
2017-03-27 10:56:23 +0200

    Add file array completed.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# JS-exercice
+#JS-exercice
+
+* Forkez le repo !
+* Des que vous validé une étape, ajoutez le et commitez le.
+
+# Add File array.
---
+++ array/app.js
@@ -0,0 +1,53 @@
+//var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+
+// Exercice n°1
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+
+// Exercice n°2
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+console.log (mois[2]);
+
+// Exercice n°3
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+console.log (mois[5]);
+
+// Exercice n°4
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+console.log (mois[10]);
+
+// Exercice n°5
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+mois.splice(7, 1);
+mois.splice(7, 0, "août");
+console.log (mois);
+
+// Exercice n°6
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre"]
+console.log (mois);
+
+// Exercice n°7
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+console.log (array);
+
+// Exercice n°8
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+array.push("Courgette")
+console.log (array);
+
+// Exercice n°9
+var array = ['Pomme', 'Cerise', 'Tomate', 'Citron', [ 'Orange', 'Poivron' ], 'Courgette'];
+array.splice(4, 0, "Citron");
+console.log (array);
+
+// Exercice n°10
+var array = ['Pomme', 'Cerise', 'Tomate', 'Citron', 'Citron', [ 'Orange', 'Poivron' ], 'Courgette'];
+array.splice(1, 1);
+console.log (array);
+
+
+// Exercice n°11
+var mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+array.splice(2, 0, "Poire");
+console.log (array);
+
+
